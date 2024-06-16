b8e5c02a0a06716136b77c9b63f2325ec5205e19
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-06-16 22:39:36 +0900

    Create Xcode Project

---
+++ HitTheSpot/HitTheSpot/HitTheSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  HitTheSpotApp.swift
+//  HitTheSpot
+//
+//  Created by 남유성 on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct HitTheSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
