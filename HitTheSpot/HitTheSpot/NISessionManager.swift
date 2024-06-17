64e3650569e6578a0647953bebcb54f7c7adf119
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-06-17 21:21:15 +0900

    Feat: #1 NISessionManager 생성 및 NISessionDelegate 추가

---
+++ HitTheSpot/HitTheSpot/NISessionManager.swift
@@ -0,0 +1,123 @@
+//
+//  NISessionManager.swift
+//  HitTheSpot
+//
+//  Created by 남유성 on 6/17/24.
+//
+
+import Foundation
+import NearbyInteraction
+
+@Observable
+class NISessionManager: NSObject {
+    @ObservationIgnored private var peerDiscoveryToken: NIDiscoveryToken?
+}
+
+extension NISessionManager: NISessionDelegate {
+    // MARK: - 세션의 피어 모니터링
+    
+    /// NearBy 객체의 Session이 업데이트될 때 호출
+    /// - Parameters:
+    ///   - session: 업데이트 된 객체의 세션
+    ///   - nearbyObjects: 업데이트된 NearBy 객체의 리스트
+    func session(
+        _ session: NISession,
+        didUpdate nearbyObjects: [NINearbyObject]
+    ) {
+        // Ensure there's a current peer token.
+        guard let peerToken = peerDiscoveryToken else {
+            fatalError("don't have peer token")
+        }
+        
+        // Find the right peer from session update.
+        guard let peerObj = nearbyObjects.first (where: { $0.discoveryToken == peerToken }) else {
+            return
+        }
+        
+        // MARK: - 피어의 거리 Meter 단위
+        // Retrieve the peer's distance, in meters.
+        if let distance = peerObj.distance {
+            // Compute peer object's distance.
+        }
+        
+        // MARK: - 피어의 수평 각도 Radian 단위
+        // Retrieve the peer's horizontal angle, in radians.
+        if let horizontalAngle = peerObj.horizontalAngle {
+            // Compute peer object's angle.
+        }
+        
+        // MARK: - 피어의 방향 simd_Float3 단위
+        // Retrieve the peer's direction, in `simd_float3`.
+        if let direction = peerObj.direction {
+            // Compute peer object's direction.
+        }
+    }
+    
+    /// 1개 이상의 NearBy 객체가 제거될 때 호출
+    /// - Parameters:
+    ///   - session: 추적으로부터 제거된 객체의 세션
+    ///   - nearbyObjects: 제거된 NearBy 객체
+    ///   - reason: 제거된 이유
+    func session(
+        _ session: NISession,
+        didRemove nearbyObjects: [NINearbyObject],
+        reason: NINearbyObject.RemovalReason
+    ) {
+        // Only retry if the peer timed out.
+        guard reason == .timeout else { return }
+        
+        // The session runs with one accessory.
+        guard let peer = nearbyObjects.first else { return }
+        
+        
+//        if shouldResume(peer) {
+//            // Restart the session.
+//            if let config = session.configuration {
+//                session.run(config)
+//            }
+//        }
+    }
+    
+    // MARK: - 세션의 중단 관리
+    
+    /// 일시 중단된 세션을 알려주는 함수
+    /// 일단 백그라운드로 전환되면 세션을 일시 중지함.
+    /// - Parameter session: 일시 중단한 세션
+    func sessionWasSuspended(_ session: NISession) {}
+    
+    /// 세션의 일시 중단이 종료되었음을 알려주는 함수
+    /// - Parameter session: 일시 중단한 세션
+    func sessionSuspensionEnded(_ session: NISession) {}
+    
+    
+    // MARK: - 세션의 오류 처리
+    
+    /// 무효화된 세션을 알려주는 함수
+    /// - Parameters:
+    ///   - session: 무효화된 세션
+    ///   - error: 무효화된 에러 타입
+    func session(
+        _ session: NISession,
+        didInvalidateWith error: Error
+    ) {
+        
+    }
+    
+    
+    // MARK: - Coaching The User
+    /// Camera Assistance 프레임워크를 이용하기 위한 권장사항을 알려주는 함수?
+    /// - Parameters:
+    ///   - session: Camera Assistance를 활용하는 세션
+    ///   - convergence: Camera Assistance 프레임워크의 상태 및 사용자 권장 사항
+    ///   - object: 피어 장치 또는 타사 액세서리
+    func session(
+        _ session: NISession,
+        didUpdateAlgorithmConvergence convergence: NIAlgorithmConvergence,
+        for object: NINearbyObject?
+    ) {
+        
+    }
+}
+
+
+
