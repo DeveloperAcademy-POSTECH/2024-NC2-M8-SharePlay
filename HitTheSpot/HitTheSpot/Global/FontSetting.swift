2767de19afd0c498710cc806f183a09f24a60632
Gilah <kblack98@naver.com>
2024-06-19 22:20:33 +0900

    Font Style 수정 및 폰트 적용 테스트

---
+++ HitTheSpot/HitTheSpot/Global/FontSetting.swift
@@ -0,0 +1,21 @@
+//
+//  FontSetting.swift
+//  HitTheSpot
+//
+//  Created by Geunhye on 6/19/24.
+//
+
+import SwiftUI
+
+extension Font {
+    
+    static let pretendard18: Font = .custom("Pretendard-SemiBold", size: 18)
+    static let pretendard20: Font = .custom("Pretendard-SemiBold", size: 20)
+    static let pretendard24: Font = .custom("Pretendard-SemiBold", size: 24)
+    
+    static let syncopateBold70: Font = .custom("Syncopate-Bold", size: 70)
+    static let syncopateRegular70: Font = .custom("Syncopate-Regular", size: 70)
+
+    static let syncopateRegular40: Font = .custom("Syncopate-Regular", size: 40)
+
+}
