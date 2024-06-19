328ac94a7c61ddda7c3c48c9b2c4f3776726376a
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-06-19 18:00:42 +0900

    Feat: #7 MapKit UI 구현

--- HitTheSpot/HitTheSpot/Scene/Main/MainLocationView.swift
+++ HitTheSpot/HitTheSpot/Scene/Main/MainLocationView.swift
@@ -6,18 +6,86 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MainLocationView: View {
     var modeChangeHandler: (() -> Void)?
     
+    let peerName: String = "라뮤"
+    
     var body: some View {
-        ShowDistanceViewButton {
-            modeChangeHandler?()
+        ZStack {
+            Map()
+            
+            RadialGradientCover()
+            
+            VStack {
+                TitleLabel(pearName: peerName)
+                
+                Spacer()
+                
+                ShowDistanceViewButton {
+                    modeChangeHandler?()
+                }
+            }
+            .padding(.vertical, 60)
         }
     }
 }
 
 extension MainLocationView {
+    @ViewBuilder
+    func RadialGradientCover() -> some View {
+        ZStack {
+            // 그라디언트 처리
+            RadialGradient(
+                gradient: Gradient(
+                    colors: [
+                        .black.opacity(0),
+                        .black
+                    ]
+                ),
+                center: .center,
+                startRadius: 0,
+                endRadius: 200
+            )
+            .opacity(0.8)
+            .ignoresSafeArea()
+            
+            // 원
+            Circle()
+                .fill(Color.white)
+                .blendMode(.destinationOut)
+                .frame(width: 360, height: 360)
+            
+            // stroke
+            Circle()
+                .stroke(.white, lineWidth: 1)
+                .frame(width: 360, height: 360)
+        }
+        .compositingGroup()
+    }
+    
+    @ViewBuilder
+    func TitleLabel(pearName: String) -> some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 12) {
+                Text("지도에서")
+                HStack {
+                    Text("나와 \(pearName)의")
+                    Text("위치를 확인하세요")
+                        .foregroundStyle(.green)
+                }
+            }
+            .foregroundStyle(.white)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .padding(.leading, 24)
+            
+            Spacer()
+        }
+    }
+    
     @ViewBuilder
     func ShowDistanceViewButton(action: @escaping () -> Void) -> some View {
         Button {
@@ -38,5 +106,4 @@ extension MainLocationView {
 
 #Preview {
     MainLocationView()
-        .preferredColorScheme(.dark)
 }
